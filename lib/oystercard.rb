1fcfb4d544f7caee74ad440bf457e41c29235581
charlierdm <charlie.davismiller@gmail.com>
2021-02-08 17:24:52 +0000

    step6top_up limit added and test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
+	LIMIT = 90
 	attr_reader :balance
-	
+
 	def initialize
 		@balance = 0
 	end
 
 	def top_up(amount)
+		fail "You cannot top up more than £#{LIMIT}" if amount + @balance > LIMIT
+		@balance += amount
 	end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,14 +1,21 @@
 require 'oystercard'
 
 describe Oystercard do
+
 	describe '#initialize' do
-		it 'should have a default balance of zero' do 	
+		it 'should have a default balance of zero' do
 			expect(subject.balance).to eq(0)
 		end
 	end
 
 	describe '#top_up' do
-		it {is_expected.to respond_to(:top_up).with(1).argument}
+		it { is_expected.to respond_to(:top_up).with(1).argument }
+
+		it "raises an error when limit is exceeded" do
+			max = Oystercard::LIMIT
+			subject.top_up(max)
+			expect{ subject.top_up 1 }.to raise_error "You cannot top up more than £#{max}"
+		end
 	end
 
 end
