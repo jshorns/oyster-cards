1d4804d104458a11ab4983c8af350ed78e2b09f9
Aniruddh <ojhaa94@gmail.com>
2021-02-08 16:16:30 +0000

    adds balance and first test

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,5 +1,9 @@
 require 'oystercard'
 
 describe Oystercard do
-
+	describe '#initialize' do
+		it 'should have a default balance of zero' do 	
+			expect(subject.balance).to eq(0)
+		end
+	end
 end
